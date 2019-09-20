f49fb78994101eb662a6295497e3fe8d1e0de4ce
Mathias Flinta <flintax5@gmail.com>
2019-09-20 16:36:42 +0200

    Update of data and outline

--- datawrangling.R
+++ datawrangling.R
@@ -1,4 +1,15 @@
 # Data wrangling
 library(readr)
-data <- read_csv("Data/cwurData.csv")
-View(data)
+library(tidyverse)
+data <- read_csv("~/Dropbox/lending-club-loan-data/loan.csv")
+
+# Variables to select
+
+vars <- c("loan_amnt", "funded_amnt", "funded_amnt_inv", "term", "int_rate")
+
+# View of data
+data %>% 
+  filter(loan_status != "Current") %>% 
+  select(vars) %>% 
+  head()
+
