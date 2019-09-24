4f12f652d148cc94b87891fc632d5052b7333ca0
skorsg15 <skorsg15@student.aau.dk>
2019-09-24 12:11:42 +0200

    Opdatering af variable

--- Signe.R
+++ Signe.R
@@ -1,1 +1,12 @@
 #Michael se
+
+setwd("~/Desktop/R studio")
+install.packages("tidyverse")
+install.packages("dplyr")
+library(tidyverse)
+library(dplyr)
+
+library(readr)
+data <- read_delim("~/Documents/5. semester/Makroøkonomi 3/data.txt", 
+                   "\t", escape_double = FALSE, trim_ws = TRUE)
+View(data)
